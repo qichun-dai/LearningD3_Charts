a9f50ef2b32d0b4870c42f34a0035db627ddc0b5
Qichun Dai <49536404+qichun-dai@users.noreply.github.com>
2023-07-18 23:37:30 +0200

    updated bar charts

--- Bar Charts/js/main.js
+++ Bar Charts/js/main.js
@@ -1,20 +1,17 @@
 async function drawChart() {
     // read data
-    const dataset = await d3.json("./data/unemployment.json")
+    const dataset = await d3.csv("./data/Stations.csv")
     console.log(dataset)
 
     
-    console.log(dataset[0].month)
+    console.log(dataset[0].location)
 
-    const dateParse = d3.timeParse("%m/%d/%Y")
-    const xAccessor = d => dateParse(d.date)
-    const yAccessor =  d => d.unemployment
+    const xAccessor = d => d.frequency
+    const yAccessor =  d => d.location
 
-    console.log(dateParse(dataset[0].date))
-
-    const margin = { left: 100, right: 15, top: 10, bottom: 50 }
+    const margin = { left: 200, right: 50, top: 10, bottom: 50 }
     const svgWidth = window.innerWidth * 0.6
-    const svgHeight = svgWidth * 0.5
+    const svgHeight = window.innerHeight *0.9
     const chartWidth = svgWidth - margin.left - margin.right
     const chartHeight = svgHeight - margin.top - margin.bottom
 
@@ -28,104 +25,88 @@ async function drawChart() {
       .style("transform", 
       `translate(${margin.left}px, ${margin.top}px)`)
 
-    const xScale = d3.scaleTime()
-        .domain(d3.extent(dataset, xAccessor))
+    const xScale = d3.scaleLinear()
+        .domain([0,d3.max(dataset, xAccessor)])
         .range([0,chartWidth])
 
-    const yScale = d3.scaleLinear()
-        .domain(d3.extent(dataset, yAccessor))
-        .range([chartHeight, 0])
+    const yScale = d3.scaleBand()
+        .domain(d3.range(dataset.length))
+        .range([0, chartHeight])
+        .round(true)
+        .padding(0.25)
 
-    const startLineGenerator = d3.line()
-        .x(d => xScale(xAccessor(d)))
-        .y(chartHeight)
-    
-        
-    const endLineGenerator = d3.line()
-    .x(d => xScale(xAccessor(d)))
-    .y(d => yScale(yAccessor(d)))
+    const bar = chart.append("g")
+                .attr("fill","#FFC917")
+                .selectAll("rect")
+                .data(dataset)
+                .join("rect")
+                .attr("class", yAccessor)
+                .attr("x", xScale(0))
+                .attr("y", (d, i) => yScale(i))
+                .attr("width", d => xScale(xAccessor(d)) - xScale(0))
+                .attr("height", yScale.bandwidth())
 
-    const line = chart.append("path")
-        .attr("class","line")
-        .attr("d", startLineGenerator(dataset))
-        .attr("fill","none")
-        .attr("stroke","gray")
-        .attr("stroke-width",1.5)
-        .transition().duration(1000)
-        .attr("d", endLineGenerator(dataset))
-        
+    chart.append("g")
+    .attr("text-anchor", "end")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 16)
+    .attr('font-weight', 600)
+    .selectAll("text")
+    .data(dataset)
+    .join("text")
+    .attr("x", d => xScale(0))
+    .attr("y", (d, i) => yScale(i) + yScale.bandwidth() / 2)
+    .attr("dy", "0.35em")
+    .attr("dx", -4)
+    .attr("fill", "#003082")
+    .text(d => yAccessor(d))
 
-    chart.append('rect')
-    .attr('width', chartWidth)
-    .attr('height', chartHeight)
-    .style('opacity', 0)
-    .on('touchmouse mousemove', function(event){
-      const mousePos = d3.pointer(event, this)
-      
-      // x coordinate stored in mousePos index 0
-      const date = xScale.invert(mousePos[0])
-      const index = d3.bisect(dataset, date)
-
-      const dateBisector = d3.bisector(xAccessor).left
-      const bisectionIndex = dateBisector(dataset, date)
-      const hoveredIndexData = dataset[bisectionIndex - 1]
-
-      console.log(hoveredIndexData)
-      tooltipDot.style('opacity', 1)
-        .attr('cx', xScale(xAccessor(hoveredIndexData)))
-        .attr('cy', yScale(yAccessor(hoveredIndexData)))
-
-        tooltip
-        .style("display", "block")
-        .style("top", `${yScale(yAccessor(hoveredIndexData)) - 60}px`)
-        .style("left", `${xScale(xAccessor(hoveredIndexData))+ window.innerWidth * 0.2}px`);
-
-        tooltip.select(".unemployment").text(`${yAccessor(hoveredIndexData)}%`);
-
-        const dateFormatter = d3.timeFormat("%B %-d, %Y");
-
-        tooltip.select(".date").text(`${dateFormatter(xAccessor(hoveredIndexData))}`)
-    })
-    .on("mouseleave", function () {
-      tooltipDot.style("opacity", 0);
-      tooltip.style("display", "none");
-    });
+    chart.append("g")
+    .attr("text-anchor", "start")
+    .attr("font-family", "sans-serif")
+    .attr("font-size", 16)
+    .attr('font-weight', 600)
+    .selectAll("text")
+    .data(dataset)
+    .join("text")
+    .attr("x", d => xScale(xAccessor(d)))
+    .attr("y", (d, i) => yScale(i) + yScale.bandwidth() / 2)
+    .attr("dy", "0.35em")
+    .attr("dx", +4)
+    .attr("fill", "#003082")
+    .text(d => xAccessor(d))
     
     
-    const tooltip = d3.select("#tooltip")
 
-    const tooltipDot = chart
-        .append("circle")
-        .attr("r", 5)
-        .attr("fill", "#fc8781")
-        .attr("stroke", "black")
-        .attr("stroke-width", 2)
-        .style("opacity", 0)
-        .style('pointer-events', 'none')
+
+
+    
+                
+
     
 
-    const xAxis = chart.append("g")
-        .attr("class", "x-axis")
-        .attr("transform", `translate(0,${svgHeight-margin.bottom})`)
-        .call(d3.axisBottom().scale(xScale))
-        .call(g => g.append("text")
-            .attr("x", chartWidth/2)
-            .attr("y", margin.bottom-12)
-            .attr("fill", "#666")
-            .attr("text-anchor", "end")
-            .text("Date"))
+    // const xAxis = chart.append("g")
+    //     .attr("class", "x-axis")
+    //     .attr("transform", `translate(0,${svgHeight-margin.bottom})`)
+    //     .call(d3.axisBottom().scale(xScale))
+    //     .call(g => g.append("text")
+    //         .attr("x", chartWidth/2)
+    //         .attr("y", margin.bottom-12)
+    //         .attr("fill", "#666")
+    //         .attr("text-anchor", "end")
+    //         .text("Date"))
 
-    const yAxis = chart.append("g")
-        .attr("class", "y-axis")
-        .attr("transform", `translate(${-10},0)`)
-        .call(d3.axisLeft().scale(yScale.nice()))
-        .call(g => g.append("text")
-        .attr('transform', `rotate(-90,${-margin.left/2},${chartHeight/2})`)
-            .attr("x", -margin.left/2)
-            .attr("y", chartHeight/2)
-            .attr("fill", "#666")
-            .attr("text-anchor", "middle")
-            .text("Unemployment Rate"))
+    // const yAxis = chart.append("g")
+    //     .attr("class", "y-axis")
+    //     .attr("transform", `translate(${-10},0)`)
+    //     .call(d3.axisLeft().scale(yScale.nice()))
+    //     .call(g => g.append("text")
+    //     .attr('transform', `rotate(-90,${-margin.left/2},${chartHeight/2})`)
+    //         .attr("x", -margin.left/2)
+    //         .attr("y", chartHeight/2)
+    //         .attr("fill", "#666")
+    //         .attr("text-anchor", "middle")
+    //         .text("Unemployment Rate"))
    
 } 
 
