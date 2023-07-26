9c6a14d26dcc2eee7cf273f71162e62501fb9bba
Qichun Dai <49536404+qichun-dai@users.noreply.github.com>
2023-07-26 13:47:57 +0200

    update maps and geojson

--- Maps/js/main.js
+++ Maps/js/main.js
@@ -25,17 +25,75 @@ async function drawMap() {
     .center([-99.9018, 33.9686])
     const path = d3.geoPath().projection(projection)
 
-    pathGenerator = d3.geoPath().projection(projection)
-
     
     // read data
 
     const geojson = await d3.json("./data/texas.json")
     
-    chart.append("path")
-     .attr("d", pathGenerator(geojson))
-     .attr("fill", "none") // This removes the fill color
-     .attr("stroke", "#666")
+
+    console.log(geojson)
+
+    // function waterCategory(value) {
+    //     if (value < 1000) return "low"   // replace 1000 with your desired threshold
+    //     if (value < 5000) return "medium" // replace 5000 with your desired threshold
+    //     return "high"
+    // }
+    
+    // geojson.geometries.forEach((geometry, index) => {
+    //     if (dataset[index]) {
+    //         geometry.properties = geometry.properties || {}
+    //         geometry.properties.AWATERCategory = waterCategory(+dataset[index].AWATER)
+    //     }
+    // })
+    
+    // const colorScale = d3.scaleOrdinal()
+    // .domain(["low", "medium", "high"]) 
+    // .range(["#a6cee3", "#1f78b4", "#b2df8a"])
+    
+    // chart.selectAll("path")
+    // .data(geojson.geometries) 
+    // .enter().append("path")
+    // .attr("d", path)
+    // .attr("fill", d => {
+    //     if (d.properties && d.properties.AWATERCategory) {
+    //         return colorScale(d.properties.AWATERCategory)
+    //     } else {
+    //         return "#ccc";  // default color if no category is found
+    //     }
+    // })
+    // .attr("stroke", "#666")
+    // .attr("stroke-width", "2")
+    // .attr("fill-opacity", 0.2)
+
+    // Set up the color scale
+    const colorScale = d3.scaleSequential()
+        .domain(d3.extent(geojson.features, d => d.properties.AWATER))
+        .interpolator(t => d3.interpolateBlues(0.1 + t * 0.9 ))
+
+    const tooltip = d3.select("#tooltip")
+    const formatNumber = d3.format(",")
+    // Draw the map polygons
+    chart.selectAll("path")
+        .data(geojson.features)
+        .enter().append("path")
+        .attr("d", path)
+        .attr("fill", d => colorScale(d.properties.AWATER))
+        .attr("stroke", "#666")
+        .on("mouseover", function(event,d) {
+            tooltip
+                .style("left", (event.pageX -50) + "px")
+                .style("top", (event.pageY-margin.top-50) + "px")
+                .style("display", "inline-block")
+                .html(`AWATER: ${formatNumber(d.properties.AWATER)}`)
+        })
+        .on("mousemove", function(event,d) {
+            tooltip
+                .style("left", (event.pageX -50) + "px")
+                .style("top", (event.pageY-margin.top-50 ) + "px")
+        })
+        .on("mouseout", function() {
+            tooltip.style("display", "none")
+        })
      
 } 
 
