291eb5c534ce42350d09f953af95a8373655f9bc
Qichun Dai <qichun.dai@gmail.com>
2023-07-29 00:22:58 +0200

    update data

--- Radial Chart/data/photos.R
+++ Radial Chart/data/photos.R
@@ -1,9 +1,41 @@
 library(tidyverse)
-path_to_directory <- setwd("D:/Photo/2019-01/")
+
+# Step 1: List all directories starting with "2019"
+root_directory <- "D:/Photo/"  # Replace with your folder path
+all_dirs <- list.dirs(path = root_directory, full.names = TRUE, recursive = FALSE)
+
+# Filter directories starting with "2019"
+dirs_2019 <- all_dirs[grep("^2019", basename(all_dirs))]
+
+print(dirs_2019)
+
+# Step 2: For each directory, get the photo information
+photo_data <- lapply(dirs_2019, function(dir_path) {
+  photo_files <- list.files(path = dir_path, pattern = "\\.(jpg|jpeg|png|bmp|tiff)$", full.names = TRUE, ignore.case = TRUE)
+  if(length(photo_files) > 0) {
+    file_info <- file.info(photo_files)
+    return(data.frame(
+      FileName = rownames(file_info),
+      CreatedTime = file_info$ctime,
+      LastModified = file_info$mtime,
+      AccessTime = file_info$atime
+    ))
+  } else {
+    return(data.frame( FileName = character(), CreatedTime = as.POSIXct(character()), LastModified = as.POSIXct(character()), AccessTime = as.POSIXct(character())))
+  }
+})
+
+# Combine all data frames into one
+photo_data_combined <- bind_rows(photo_data) %>% 
+  filter(startsWith(format(CreatedTime, "%Y-%m-%d %H:%M:%S"), "2019"))
+
+if (requireNamespace("rstudioapi", quietly = TRUE)) {
+  script_path <- rstudioapi::getSourceEditorContext()$path
+  print(script_path)
+  dir_only <- sub("/[^/]+$", "", script_path)
+  setwd(dir_only)
+}
+
+write.csv(photo_data_combined, file = "./photos2019.csv", row.names = FALSE)
 
 
-photo_files <- list.files(path = "path_to_directory", pattern = "\\.(jpg|jpeg|png|bmp|tiff)$", full.names = TRUE, recursive = FALSE, ignore.case = TRUE)
-
-print(photo_files)
-
-print(file.info(path_to_directory))
