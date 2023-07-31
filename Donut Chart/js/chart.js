ca78c5930fc56f03ca680966fbe4ad5aab6f88f2
Qichun Dai <49536404+qichun-dai@users.noreply.github.com>
2023-07-31 22:59:54 +0200

    update donut chart

---
+++ Donut Chart/js/chart.js
@@ -0,0 +1,150 @@
+async function drawPie() {
+
+  // 1. Access data
+
+  const dataset = await d3.csv("./data/amsterdam 2022.csv")
+  console.log(dataset)
+
+    // Count frequency of each icon value
+  const iconFrequency = dataset.reduce((acc, curr) => {
+    acc[curr.icon] = (acc[curr.icon] || 0) + 1
+    return acc
+  }, {})
+
+  // Convert frequency object to an array for D3
+  const iconArray = Object.entries(iconFrequency).map(([key, value]) => ({
+    icon: key,
+    count: value
+  }))
+
+  
+
+  // 2. Create chart dimensions
+
+  const width = 550
+  let dimensions = {
+    width: width,
+    height: width,
+    margin: {
+      top: 60,
+      right: 60,
+      bottom: 60,
+      left: 60,
+    },
+  }
+  dimensions.boundedWidth = dimensions.width - dimensions.margin.left - dimensions.margin.right
+  dimensions.boundedHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom
+
+  // 3. Draw canvas
+
+  const wrapper = d3.select("#wrapper")
+    .append("svg")
+      .attr("width", dimensions.width)
+      .attr("height", dimensions.height)
+
+  const bounds = wrapper.append("g")
+      .style("transform", `translate(${dimensions.boundedWidth / 2}px, ${dimensions.boundedHeight / 2}px)`)
+
+  // 4. Create scales
+
+
+
+
+const radius = Math.min(dimensions.boundedWidth, dimensions.boundedHeight) / 2 -10
+const donutWidth = 60
+
+const color = d3.scaleOrdinal(d3.schemeCategory10)
+
+
+const arc = d3.arc()
+  .innerRadius(radius - donutWidth)
+  .outerRadius(radius);
+
+  const pie = d3.pie()
+  .value(d => d.count)
+  .sort((a, b) => b.count - a.count)
+
+const weatherConditions = {
+  "clear-day": '#FFF56C',  
+  "cloudy": '#D8D9DA',   
+  "partly-cloudy-day": '#937DC2', 
+  "rain": '#CEE6F3',      
+  "snow": '#FBA1B7',      
+  "fog": '#D7BBF5'       
+}
+
+const weatherNames = {
+  "clear-day": 'Sunny',  
+  "cloudy": 'Cloudy',   
+  "partly-cloudy-day": 'Partly Cloudy', 
+  "rain": 'Rainy',      
+  "snow": 'Snowy',      
+  "fog": 'Foggy'       
+}
+
+// Function to get color based on condition
+function getWeatherColor(condition) {
+    return weatherConditions[condition];
+}
+
+function getNames(condition) {
+  return weatherNames[condition];
+}
+
+const path = bounds.selectAll("path")
+  .data(pie(iconArray))
+  .enter()
+  .append("path")
+  .attr("d", arc)
+  .attr("fill", d => getWeatherColor(d.data.icon))
+
+  // 6. Draw peripherals
+
+  bounds.append("text")
+      .attr("class", "title")
+      .text("2022 Weather")
+     
+
+  bounds.append("text")
+      .attr("class", "title-small")
+      .text("Amsterdam, NL")
+      .attr("transform", `translate(0, 40)`)
+
+  // Add labels to the arcs using the centroid function
+  const labels = bounds.selectAll(".label")
+  .data(pie(iconArray))
+  .enter()
+  .append("text")
+  .attr("transform", function(d) {
+      // Use arc.centroid() to get the label's x and y positions
+      const [x, y] = arc.centroid(d);
+      return `translate(${x * 1},${y * 1})`;  // Multiplying by 1.5 pushes the labels out
+  })
+  .attr("text-anchor", "middle")
+  .each(function(d) {
+        // Check if count is above 20
+        if (d.data.count <= 20) {
+            d3.select(this).remove();  // Remove the text element if the condition isn't met
+            return;  // Exit this iteration of the loop
+        }
+
+        const label = d3.select(this);
+        
+        // Add the weather condition
+        label.append("tspan")
+        .attr("class","legend")
+            .attr("x", 0)
+            .attr("y", "-0.6em")  // Slightly offset to accommodate for the second line
+            .text(getNames(d.data.icon))
+      
+        
+        // Add the count on a new line
+        label.append("tspan")
+        .attr("class","legend")
+            .attr("x", 0)
+            .attr("y", "1em")  // Positioning for the second line
+            .text(d.data.count)
+    });
+
+}
+drawPie()
---
+++ Line Charts Brush Scroll/js/main.js
@@ -0,0 +1,260 @@
+async function drawChart() {
+    // read data
+    const dataset = await d3.json("./data/unemployment.json")
+    console.log(dataset)
+
+    
+    console.log(dataset[0].month)
+
+    const dateParse = d3.timeParse("%m/%d/%Y")
+    const xAccessor = d => dateParse(d.date)
+    const yAccessor =  d => d.unemployment
+
+    console.log(dateParse(dataset[0].date))
+
+    const margin = { 
+        left: 45, 
+        right: 60, 
+        top: 10, 
+        bottom: 150 }
+    const margin2 = {
+        left: 45, 
+        right: 60, 
+        top: 490, 
+        bottom: 50
+      }
+    const svgWidth = 900
+    const svgHeight = 600
+    const chartWidth = svgWidth - margin.left - margin.right
+    const chartHeight = svgHeight - margin.top - margin.bottom
+    const chartHeight2 = svgHeight - margin2.top - margin2.bottom
+
+
+   
+
+
+
+    
+
+    const xScale = d3.scaleTime()
+        .domain(d3.extent(dataset, xAccessor))
+        .range([0,chartWidth])
+
+    const xAxisGenerator = d3.axisBottom()
+        .scale(xScale)
+
+    const xScale2 = d3.scaleTime()
+        .domain(d3.extent(dataset, xAccessor))
+        .range([0,chartWidth])
+
+    const xAxisGenerator2 = d3.axisBottom()
+        .scale(xScale2)
+
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(dataset, yAccessor))
+        .range([chartHeight, 0])
+    
+    const yScale2 = d3.scaleLinear()
+        .domain(d3.extent(dataset, yAccessor))
+        .range([chartHeight2, 0])
+    
+    const yAxisGenerator = d3.axisLeft()
+        .scale(yScale)
+    
+    const svg = d3.select("#chart-area")
+        .append("svg")
+        .attr("height", svgHeight)
+        .attr("width", svgWidth)
+    
+    const chart = svg.append("g")
+          .style("transform", 
+          `translate(${margin.left}px, ${margin.top}px)`)
+    
+    const focus = chart.append("g")
+      .attr("class", "focus")
+      .style("transform",`translate(
+        ${margin.left}px,
+        ${margin.top}px
+      )`)
+    
+      console.log(focus)
+    
+      const context = chart.append("g")
+      .attr("class", "context")
+      .style("transform", `translate(
+        ${margin2.left}px,
+        ${margin2.top}px
+      )`)
+    
+      console.log(context)
+
+    const startLineGenerator = d3.line()
+        .x(d => xScale(xAccessor(d)))
+        .y(chartHeight)
+    
+    const endLineGenerator = d3.line()
+    .x(d => xScale(xAccessor(d)))
+    .y(d => yScale(yAccessor(d)))
+
+
+
+    const LineGenerator2 = d3.line()
+        .x(d => xScale2(xAccessor(d)))
+        .y(d => yScale2(yAccessor(d)))
+
+    focus.append("clipPath")
+        .attr("id", "clip")
+        .append("rect")
+        .attr("width", chartWidth)
+        .attr("height", chartHeight)
+
+    const line = focus.append("path")
+        .attr("class","line")
+        .attr("d", endLineGenerator(dataset))
+        .attr("fill","none")
+        .attr("stroke","gray")
+        .attr("stroke-width",1.5)
+        .attr("clip-path","url(#clip)")
+        // .transition().duration(1000)
+        // .attr("d", endLineGenerator(dataset))
+        // adding animation doesn't work with brush
+    
+    const line2 = context.append("path")
+        .attr("class","line")
+        .attr("d", LineGenerator2(dataset))
+        .attr("fill","none")
+        .attr("stroke","gray")
+        .attr("stroke-width",1)
+    
+
+    const xAxis = focus.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${svgHeight-margin.bottom})`)
+        .call(d3.axisBottom().scale(xScale))
+        
+
+    const xAxis2 = context.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${chartHeight2+5})`)
+        .call(d3.axisBottom().scale(xScale2))
+        .call(g => g.append("text")
+            .attr("x", chartWidth/2)
+            .attr("y", 30)
+            .attr("fill", "#666")
+            .attr("text-anchor", "end")
+            .text("Date"))
+        
+
+    const yAxis = focus.append("g")
+        .attr("class", "y-axis")
+        .attr("transform", `translate(${-10},0)`)
+        .call(d3.axisLeft().scale(yScale.nice()))
+        .call(g => g.append("text")
+        .attr('transform', `rotate(-90,${-margin.left/2},${chartHeight/2})`)
+            .attr("x", -margin.left)
+            .attr("y", chartHeight/2)
+            .attr("fill", "#666")
+            .attr("text-anchor", "middle")
+            .text("Unemployment Rate"))
+
+    const brush = d3.brushX()
+    .handleSize(10)
+     .extent([[0,0],[chartWidth,chartHeight2+4]])
+    .on("start brush end", brushed)
+
+
+
+    const handle = context.selectAll(".custom-handle")
+    .data([{ type: "w" }, { type: "e" }])
+    .enter().append("path")
+    .attr("class", "custom-handle")
+    .attr("fill", "#666")
+    .attr("fill-opacity", 0.8)
+    .attr("stroke", "#000")
+    .attr("stroke-width", 1.5)
+    .attr("cursor", "ew-resize")
+    .attr("d", d3.arc()
+        .innerRadius(0)
+        .outerRadius(chartHeight2 / 2)
+        .startAngle(d => { return d.type === "w" ? Math.PI : 0; })
+        .endAngle(d => { return d.type === "w" ?  2 * Math.PI: Math.PI; }))
+    .attr("display", "none")
+    //set this in css not working, why?
+    
+    context.append("g")
+    .attr("class", "brush")
+    .call(brush)
+
+    
+    function brushed(event) {
+        let s0, s1;
+    
+        if (event.selection === null) {
+            xScale.domain(d3.extent(dataset, xAccessor))
+            focus.select(".x-axis")
+                .call(d3.axisBottom().scale(xScale))
+            line.attr("d", endLineGenerator(dataset))
+        } else {
+            [s0, s1] = event.selection;
+    
+            xScale.domain([s0, s1].map(xScale2.invert));
+    
+            focus.select(".x-axis")
+                .call(d3.axisBottom().scale(xScale));
+    
+            line.attr("d", endLineGenerator(dataset));
+        }
+    
+        // Only set handle positions if they're defined (i.e., if a selection exists)
+        if (typeof s0 !== "undefined" && typeof s1 !== "undefined") {
+            handle.attr("display", null)
+                .attr("transform", (d, i) => `translate(${i === 0 ? s0 : s1},${chartHeight2 / 2 + 2.5})`)
+                //+2.5 because I added 5 to the x-axis as well
+        }
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    let lastScrollTop = 0;  // A variable to keep track of the last scroll position
+
+    function onScroll() {
+        console.log("Scrolled!")
+    const currentScrollTop = window.scrollY;
+    if (currentScrollTop > lastScrollTop) {  // Detect if the scroll is going downwards
+        // Define the next date you want to zoom into
+        const startDate =  new Date(2007,11,1)
+        const endDate =  new Date(2009,6,31)
+        zoomToPeriod(startDate, endDate)
+    }
+    lastScrollTop = currentScrollTop;
+    }
+
+    function zoomToPeriod(startDate, endDate) {
+        const duration = 750;  // Transition duration
+    
+        // Set the new domain based on the provided start and end dates
+        xScale.domain([startDate, endDate]);
+    
+        // Update the x-axis and line to reflect the zoomed in period
+        focus.select(".x-axis")
+            .transition().duration(duration)
+            .call(d3.axisBottom().scale(xScale));
+        line.transition().duration(duration)
+            .attr("d", endLineGenerator(dataset));
+    
+        // Add a text in the middle of the period
+        const timeDifference = Math.abs(endDate.getTime() - startDate.getTime());
+        const differenceDays = Math.ceil(timeDifference / (1000 * 3600 * 24));
+        const midDate = d3.timeDay.offset(startDate, differenceDays / 2);
+        focus.append('text')
+            .attr('x', xScale(midDate))
+            .attr('y', 0)  // Adjust this based on where you want the text
+            .text(`2008 Resession: ${d3.timeFormat("%Y-%m-%d")(startDate)} to ${d3.timeFormat("%Y-%m-%d")(endDate)}`)
+            .attr('fill', 'black')
+            .attr('text-anchor', 'middle');
+    }
+
+   
+} 
+
+drawChart()
